407b493ac7480f1dd8e87dedad2d09e3b3f9a620
Alexander Hentzsch <47180632+AlexanderHentzsch@users.noreply.github.com>
2021-05-14 17:22:14 +0200

    Style homescreen

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,32 @@
 <template>
     <v-container style="width: 100%; height: 100%" class="d-flex align-center justify-center">
-        <v-card width="350px" height="500px">
-            <v-card-title>
-                <h1 class="text-center" style="font-size: 28px; width: 100%">Bibelbücher sortieren</h1>
+        <v-card width="350px" height="500px" class="px-5 d-flex flex-column">
+            <v-card-title class="flex-grow-0 pb-12">
+                <h1 class="text-center" style="font-size: 28px; width: 100%">🎮<br/>Bibelbücher sortieren</h1>
             </v-card-title>
-            <v-card-text class="text-center">
-                <nuxt-link to="/game" style="text-decoration: none">
-                    <v-btn>Neues Spiel</v-btn>
-                </nuxt-link>
-            </v-card-text>
-            <v-card-actions class="d-block">
-                <v-row no-gutters>
+            <v-card-text class="text-center flex-grow-1">
+                <v-row>
                     <v-col>
-                        Stil
+                        <v-btn to="/game" nuxt block elevation="0" outlined tile x-large class="mb-2">Neues Spiel</v-btn>
+                        <v-btn to="/game" nuxt block elevation="0" outlined tile x-large class="mb-2">Spielverlauf</v-btn>
+                        <v-btn to="/game" nuxt block elevation="0" outlined tile x-large class="mb-2">Einstellungen</v-btn>
                     </v-col>
                 </v-row>
-                <v-btn @click="setThemeMode('dark')" fab>
-                    <v-icon>mdi-weather-night</v-icon>
-                </v-btn>
-                <v-btn @click="setThemeMode('light')" fab>
-                    <v-icon>mdi-weather-sunny</v-icon>
-                </v-btn>
+            </v-card-text>
+            <v-card-actions class="flex-grow-0 pb-8" align="end">
+                <v-row no-gutters class="text-center d-flex flex-column">
+                    <v-col class="text-uppercase">
+                        Stil
+                    </v-col>
+                    <v-col>
+                        <v-btn @click="setThemeMode('dark')" fab icon text x-large>
+                            <v-icon>mdi-weather-night</v-icon>
+                        </v-btn>
+                        <v-btn @click="setThemeMode('light')" fab icon text x-large>
+                            <v-icon>mdi-weather-sunny</v-icon>
+                        </v-btn>
+                    </v-col>
+                </v-row>
             </v-card-actions>
         </v-card>
     </v-container>
